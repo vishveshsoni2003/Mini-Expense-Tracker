751643ca3abf9e1cba9cf6957dfa6f70db68f428
Vishvesh Soni <vishveshsoni2003@gmail.com>
2026-06-04 00:20:28 +0530

    Update API URL for production

--- frontend/src/services/expenseApi.js
+++ frontend/src/services/expenseApi.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = "http://localhost:5000/api/expenses";
+const API = import.meta.env.VITE_API_URL;
 
 export const getExpenses = async () => {
     const response = await axios.get(API);
